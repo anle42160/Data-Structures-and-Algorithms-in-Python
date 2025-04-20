057782288815b3d86a4b444cc0b8b864160fdad7
Anna Le <110627248+anle42160@users.noreply.github.com>
2025-04-20 01:17:17 -0500

    Delete Data Structures directory

--- Data Structures/1.12 Exercises/Reinforcements/R-1.5.py
+++
@@ -1,6 +0,0 @@
-#R-1.5 Give a single command that computes the sum from Exercise R-1.4, relying on Python’s comprehension syntax and the built-in sum function.
-
-#Programmer: Anna Le
-
-n = 4
-print(sum(i*i for i in range(1, n)))
