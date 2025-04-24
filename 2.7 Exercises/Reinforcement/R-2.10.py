1ee2ddce13f34db2b73aaab6697f65c227b72469
Anna Le <110627248+anle42160@users.noreply.github.com>
2025-04-24 00:11:19 -0500

    Add files via upload

---
+++ 2.7 Exercises/Reinforcement/R-2.10.py
@@ -0,0 +1,68 @@
+"""
+R-2.10 Implement the neg method for the Vector class of Section 2.3.3, so
+that the expression −v returns a new vector instance whose coordinates
+are all the negated values of the respective coordinates of v.
+
+Programmer: Anna Le
+"""
+
+#Program from Textbook
+class Vector:
+    """Represent a vector in a multidimensional space."""
+
+    def __init__(self, d):
+        """Create d-dimensional vector of zeros."""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """Return the dimension of the vector."""
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        """Return jth coordinate of vector."""
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        """Set jth coordinate of vector to given value."""
+        self._coords[j] = val
+
+    def __add__(self, other):
+        """Return sum of two vectors."""
+        if len(self) != len(other):  # relies on __len__ method
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))  # start with vector of zeros
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __eq__(self, other):
+        """Return True if vector has same coordinates as other."""
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        """Return True if vector differs from other."""
+        return not self == other  # rely on existing __eq__ definition
+
+    def __str__(self):
+        """Produce string representation of vector."""
+        return '<' + str(self._coords)[1:-1] + '>'  # adapt list representation
+    
+    #Added Code
+    def __neg__(self): #this is just the for loop in the __add__ method where it will go through each entry and invert the value
+        result = Vector(len(self))  # start with vector of zeros
+        for j in range(len(self)):
+            result[j] = -self[j]
+        return result
+
+#Test
+v = Vector(3)
+v[0] = 1
+v[1] = 4
+v[2] = 2
+
+print(-v)
+
+"""
+This will invert the entries of the given vector.
+If __neg__ is not there, then it will give a TypeError.
+"""
---
+++ 2.7 Exercises/Reinforcement/R-2.13.py
@@ -0,0 +1,65 @@
+"""
+R-2.13 Exercise R-2.12 asks for an implementation of mul , for the Vector
+class of Section 2.3.3, to provide support for the syntax v 3. Implement
+the rmul method, to provide additional support for syntax 3 v.
+
+Programmer: Anna Le
+"""
+
+#Program from Textbook
+class Vector:
+    """Represent a vector in a multidimensional space."""
+
+    def __init__(self, d):
+        """Create d-dimensional vector of zeros."""
+        self._coords = [0] * d
+
+    def __len__(self):
+        """Return the dimension of the vector."""
+        return len(self._coords)
+
+    def __getitem__(self, j):
+        """Return jth coordinate of vector."""
+        return self._coords[j]
+
+    def __setitem__(self, j, val):
+        """Set jth coordinate of vector to given value."""
+        self._coords[j] = val
+
+    def __add__(self, other):
+        """Return sum of two vectors."""
+        if len(self) != len(other):  # relies on __len__ method
+            raise ValueError('dimensions must agree')
+        result = Vector(len(self))  # start with vector of zeros
+        for j in range(len(self)):
+            result[j] = self[j] + other[j]
+        return result
+
+    def __eq__(self, other):
+        """Return True if vector has same coordinates as other."""
+        return self._coords == other._coords
+
+    def __ne__(self, other):
+        """Return True if vector differs from other."""
+        return not self == other  # rely on existing __eq__ definition
+
+    def __str__(self):
+        """Produce string representation of vector."""
+        return '<' + str(self._coords)[1:-1] + '>'  # adapt list representation
+    
+    #Added Code
+    def __rmul__(self, other): #this is essentially the __mul__ method but instead of having it multiply against a vector, it's expecting 'other' to be a single int/float
+        if not isinstance(other, (int, float)):
+            raise TypeError ('dimensions must agree')
+        result = Vector(len(self))  # start with vector of zeros
+        for j in range(len(self)):
+            result[j] = other * self[j]
+        return result
+    
+#Test
+v = Vector(3)
+v[0] = 1
+v[1] = 4
+v[2] = 2
+
+print (3*v)
